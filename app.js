71b6280ce2f17b6df0cbc8318e6d7235e60ef682
arsadssss <arsadsagir@gmail.com>
2024-09-21 01:07:09 +0500

    More question has been added

--- app.js
+++ app.js
@@ -303,3 +303,211 @@
 // let b = `like`;
 // console.log(a === b);
 // output - true
+
+// 46. Why - first decreament then compare 
+// let a = 1;
+// let c = 2;
+// console.log(--c === a);
+// Output -  true
+
+// 46. Why -  after a & b compared so it become true means bollean and c is still number to it become false
+// let a = 1;
+// let b = 1;
+// let c = 2;
+// console.log(a === b === --c);
+// Output -  false
+
+// 47. why -
+// console.log(3*3) // 9
+// console.log(3**3) // 27
+// console.log(3***3); // error
+
+// 48. why - hoisting property
+// console.log(a)
+// var a;
+// output - undefined
+
+// 49. why - not declare
+// console.log(b);
+// output- not defined
+
+// 50. why - it is a nested array 
+// console.log([[[[]]]]);
+// output - nested array 
+
+// 51. how to find OS in js
+// navigator.platform
+// output - window - 32
+
+// 52. why - becoz for is a reserved keyword 
+// let for = 100;
+// output - unexpedted token for 
+
+// 53. why - 
+// function fruit(){
+//     console.log(name); // undefined due to hoisting using car
+//     console.log(price); // error becouse of let variable
+//     var name = "apple";
+//     let price = 20;
+// }
+// fruit();
+// output - 
+
+// 54. why - due to global variable and i is a global varibale here and due to setTimeout i will be stored in the stack so it will print after every process to output will be 3
+// for(var i = 0; i < 3; i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1);
+// }
+// output - 3 3 3
+
+// 55. why - let is a scope variable . In setTimeout everytime goes different i value due to scope variable.
+// for(let i = 0; i < 3; i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1);
+// }
+// output - 0 1 2
+
+// 56. why - when we add + before in any value that become number
+// console.log(+true);
+// console.log(typeof +true);
+// output - 1 | true
+
+// 57. why - because opposite of string is booelan so it become false
+// console.log(!"anil"); // false
+// console.log(typeof("anil"));  // string
+// output -  false | string
+
+// 58. why - 
+// let data = "size";
+// const bird = {
+//     size: "small",
+// };
+// console.log(bird[data]); // small
+// console.log(bird["size"]);// small
+// console.log(bird.size); // small
+// console.log(bird.data); // undefined
+
+// 59. why - becaouse here location will be copy so if location is same then value will be same
+// let c = { name : "peter"}
+// let d;
+// d = c;
+// c.name = "anil",
+// console.log(d.name);
+// output - anil
+
+// 60. why - variable (var) can be declare again and again
+// var x;
+// var x=  10;
+// console.log(x);
+// output - 10;
+
+// 61. why - a will be normal number type but b will be used as a object constructor so it is object now. 
+// let a = 3;
+// let b = new Number(3);
+
+// console.log(a == b); // true
+// console.log(a === b); // false
+// output -  true | false 
+
+// 62. why - 
+// let name;
+// nmae = {} // typo
+// console.log(name); // blank
+// console.log(nmae); // blank object
+
+// 63. why - fruit.name will not effect the function
+// function fruit(){
+//     console.log("Woof!");
+// }
+// fruit.name = "apple";
+// fruit ();
+// output - Woof!
+
+// 64. why - both will become the string
+// function sum(a, b){
+//     return a + b;
+// }
+// console.log(sum(1, "2"));
+// output - 12
+
+// 65. why - first console then post increament then increament of post increatemt then simply increamented value
+// let number = 0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
+// output 0 2 2
+
+// 66. why - coz after using spread operator it become array
+// function getAge(...args){
+//     console.log(typeof args);
+// }
+// getAge(21);
+// output - objects
+
+// 67. why - if we use 'use strict' then variable should be declare. in 'useStrick" case hoisting also will not work. 
+// function getAge(){
+//     'use strict' // now output will be error
+//     age = 20; // if we dont write any variable type then it will become var automatically
+//     console.log(age);
+// }
+// getAge();
+// output - 20
+
+// 68. why - eval function will convert string value into expression and give the evaluated value
+// const sum = eval('10*10+5');
+// console.log(sum);
+// output - 105
+
+// 69. Q - how long is cool_secret accessible?
+// sessionStorage.setItem('cool_secret', 123);
+// Ans - session storage will be remove once we close the tab. local storage will no remove untill we remove manually
+
+// 70. why - as we can't declare a variable as a number but as a property we can define number
+// const obj = {1: "a", 2: "b", 3: "c"};
+// console.log(obj.hasOwnProperty("1"));
+// console.log(obj.hasOwnProperty(1));
+// output - true | true
+
+// 71. why - position will be same but value will update
+// const obj = { a: "one", b:"two", a:"three"}
+// console.log(obj);
+// output - {a:"three", b:"two"}
+
+// 72. why - 3 will be skiped due to continue
+// for(let i = 1; i< 5; i++){
+//     if(i === 3) continue
+//     console.log(i);
+// }
+// output - 1 2 4
+
+// 73. why - due to setTimeout  it will go to stack will execute after all execution
+// const foo = () => console.log('first');
+// const bar = () => setTimeout(()=>console.log('second')) ;
+// const baz = () => ()=>console.log('third');
+// foo();
+// bar();
+// baz();
+// output - first | third | second 
+
+// 74. why - first will call child then parent.. from child to parent
+{/* <div onclick="console.log('first)">
+    <div onclick="console.log('second)">
+       <button onclick="console.log('button)">
+        click!
+       </button>
+    </div>
+</div> */}
+// output - button | second | first 
+
+// 75. why - 
+// const person = {name: 'arsad'};
+
+// function sayHi(age){
+//     return `${this.name} is ${age}`;
+// }
+
+// console.log(sayHi.call(person, 21)); // it will work perfectly
+// console.log(sayHi.bind(person, 21)); // it will return function. we have to call this function after bind then it will return value 
+
